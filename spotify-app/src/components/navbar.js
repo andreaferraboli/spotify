6ed59a26b4c976f3dd9b1d4a4a5f4541d7b43095
andre <andreaferraboli2004@gmail.com>
2023-06-04 16:20:09 +0200

    small improvements

--- spotify-app/src/components/navbar.js
+++ spotify-app/src/components/navbar.js
@@ -7,11 +7,11 @@ function MyNavbar(){
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="/">Spotify</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#features"></Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
               <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
